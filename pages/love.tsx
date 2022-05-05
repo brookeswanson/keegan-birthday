da1b9dd6dc9d0493c0f731f566953bc15b4228db
bswanson <brookeswanson09@gmail.com>
2022-05-05 08:25:49 -0500

    Formatting.

--- pages/love.tsx
+++ pages/love.tsx
@@ -1,24 +1,26 @@
-import { NextPage } from "next/types"
+import { NextPage } from "next/types";
 import { useState } from "react";
 import Gallery from "../components/Gallery";
 
 const images = [
-    {imageUrl: '/kiran-and-hammocks.png', text: 'Meow'}
-    , {imageUrl: '/kiran-and-sunshine.png', text: 'Hello'}
-    , {imageUrl: '/no-crying-in-baseball.png', text: 'Grape'}
-    , {imageUrl: '/couple-goals.png', text: 'Colleen and Emma'}
-    , {imageUrl: '/housemates.png', text: 'Park Benches and fun'}
-    , {imageUrl: '/fam.png', text: 'Love.'}
-]
+  { imageUrl: "/kiran-and-hammocks.png", text: "Meow" },
+  { imageUrl: "/kiran-and-sunshine.png", text: "Hello" },
+  { imageUrl: "/no-crying-in-baseball.png", text: "Grape" },
+  { imageUrl: "/couple-goals.png", text: "Colleen and Emma" },
+  { imageUrl: "/housemates.png", text: "Park Benches and fun" },
+  { imageUrl: "/fam.png", text: "Love." },
+  { imageUrl: "/MIB.png", text: "Aliens wooo", vertical: true },
+];
 const Love: NextPage = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    return (
+  return (
     <Gallery
-    images={images}
-    currentIndex={currentIndex}
-    setCurrentIndex={setCurrentIndex}
-         />);
-}
+      images={images}
+      currentIndex={currentIndex}
+      setCurrentIndex={setCurrentIndex}
+    />
+  );
+};
 
 export default Love;
